907f915cd1829936382bbb6b3d05e17ee9e985b9
Darshan Bankar <162464131+Darshan1915@users.noreply.github.com>
2024-09-26 21:42:18 +0530

    Time: 44 ms (6.17%), Space: 28.3 MB (10.95%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+        vector<int>temp;
+        int len=nums.size();
+        int j=0;
+        k=k%len;
+        for(int i=0;i<abs(len-k);i++)
+        {
+            //1,2,3,4
+            cout<<nums[i]<<" ";
+            temp.push_back(nums[i]);
+        }
+        cout<<endl;
+        for(int i=abs(len-k);i<len;i++)
+        {
+            //5,6,7
+
+            cout<<nums[i]<<endl;
+            nums[abs(i-abs(len-k))]=nums[i];
+        }
+        for(int i=k;i<len;i++)
+        {
+            nums[i]=temp[j];
+            j++;
+        }
+        
+        // k=k%nums.size();
+        // reverse(nums.begin(),nums.begin()+(nums.size()-k));
+
+        // reverse(nums.begin()+(nums.size()-k),nums.end());
+    
+        // reverse(nums.begin(),nums.end());
+            
+
+    // }
+
+
+
+
+
+        // int n = nums.size();
+        // if(k>n){
+        //     k = k % n;
+        // }
+         
+        // int temp = [k];
+        // for (int i=0; i< k; i++){
+        //     temp[i]=nums[i];
+        // }
+
+        // for (int i=0; i<temp.size(); i++){
+        //     cout<<temp[i];
+        // }
+         
+    }
+};
