b8ee512417088b36a1cb847fc0107ee712e96a9e
Darshan Bankar <162464131+Darshan1915@users.noreply.github.com>
2024-11-20 14:24:52 +0530

    Time: 0 ms (100%), Space: 27.4 MB (18.07%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int size = s.size();
-        int a= 0, z = (size-1);
-        // for (int i=0; i!='\0';i++){
-        //     swap()
-        // }
-        while(a<z){
-            swap(s[a++],s[z--]);
+        int st = 0;
+        int e = s.size() - 1;
+        while(st<=e){
+            swap(s[st++],s[e--]);
         }
     }
 };
