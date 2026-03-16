0c3241551423ed6182268b2457b657652a6b37aa
Darshan Bankar <162464131+Darshan1915@users.noreply.github.com>
2026-03-17 00:53:34 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.64%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        for(int j=0;j<nums.size();j++)
-        {
-            if(nums[j]!=0)
-            {
-                swap(nums[j],nums[i]);
-                i++;
+        
+        int j = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[j]);
+                j++;
             }
         }
     }
 };
+
+
+
+
+
+
+//wronggggg:-
+
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int n = nums.size();
+//         int left = 0, right = n-1;
+//         while(left<=right){
+//             if(nums[left]==0){
+//                 swap(nums[left],nums[right]);
+//                 right--;
+//             }
+//             left++;
+//         }
+//     }
+// };
